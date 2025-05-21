f1b5c282543e957095b30b63593c704c8cce5a31
Nguyen Duc Tam <tamgtasa2611@gmail.com>
2025-05-21 14:04:51 +0700

    FEAT: allow cors prod

--- src/main/java/com/jupiter/store/common/config/WebConfig.java
+++ src/main/java/com/jupiter/store/common/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000, https://jupiterpos.vercel.app")
+                        .allowedOrigins("http://localhost:3000, https://jupiterpos.vercel.app, https://jupiter-pos-dev.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
