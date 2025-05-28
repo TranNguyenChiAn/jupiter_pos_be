6f43ac3400897606363ef5646de44908ebe86f52
Nguyen Duc Tam <tamgtasa2611@gmail.com>
2025-05-28 20:01:36 +0700

    feat: trả về status product

--- src/main/java/com/jupiter/store/module/product/dto/ProductReadDTO.java
+++ src/main/java/com/jupiter/store/module/product/dto/ProductReadDTO.java
@@ -3,6 +3,7 @@ package com.jupiter.store.module.product.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.jupiter.store.common.model.AbstractAuditingEntity;
 import com.jupiter.store.module.category.model.Category;
+import com.jupiter.store.module.product.constant.ProductStatus;
 import com.jupiter.store.module.product.model.Product;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,12 +22,18 @@ public class ProductReadDTO extends AbstractAuditingEntity implements Serializab
     private Integer productId;
     private String productName;
     private String description;
+    private ProductStatus status;
     private List<Category> category;
 
     public ProductReadDTO(Product product, List<Category> productCategory) {
         this.productId = product.getId();
         this.productName = product.getProductName();
         this.description = product.getDescription();
+        this.status = product.getStatus();
         this.category = productCategory;
+        this.setCreatedBy(product.getCreatedBy());
+        this.setCreatedDate(product.getCreatedDate());
+        this.setLastModifiedBy(product.getLastModifiedBy());
+        this.setLastModifiedDate(product.getLastModifiedDate());
     }
 }
