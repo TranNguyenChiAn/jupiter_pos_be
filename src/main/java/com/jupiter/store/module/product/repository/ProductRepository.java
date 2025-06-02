98f59d4e85bd6c68a802836ec3e2555b6f4d1abf
Nguyen Duc Tam <tamgtasa2611@gmail.com>
2025-06-02 17:22:52 +0700

    fix: ko phân trang được products

--- src/main/java/com/jupiter/store/module/product/repository/ProductRepository.java
+++ src/main/java/com/jupiter/store/module/product/repository/ProductRepository.java
@@ -5,31 +5,37 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-    @Query(value = "SELECT DISTINCT p.* FROM products p " +
-            "LEFT JOIN product_variants pv ON p.id = pv.product_id " +
-            "LEFT JOIN product_categories pc ON p.id = pc.product_id " +
-            "WHERE " +
-            "(:status IS NULL OR p.status = :status) " +
-            "AND (:categoryId IS NULL OR pc.category_id = :categoryId) " +
-            "AND (LOWER(p.product_name) LIKE LOWER(CONCAT('%', :search, '%')) " +
-            "OR LOWER(p.description) LIKE LOWER(CONCAT('%', :search, '%')) " +
-            "OR LOWER(pv.sku) LIKE LOWER(CONCAT('%', :search, '%')) " +
-            "OR LOWER(pv.barcode) LIKE LOWER(CONCAT('%', :search, '%')))",
-            countQuery = "SELECT COUNT(DISTINCT p.id) FROM products p " +
+    @Query(
+            value = "SELECT DISTINCT p.* FROM products p " +
                     "LEFT JOIN product_variants pv ON p.id = pv.product_id " +
                     "LEFT JOIN product_categories pc ON p.id = pc.product_id " +
-                    "WHERE " +
-                    "(:status IS NULL OR p.status = :status) " +
+                    "WHERE (:status IS NULL OR p.status = :status) " +
                     "AND (:categoryId IS NULL OR pc.category_id = :categoryId) " +
                     "AND (LOWER(p.product_name) LIKE LOWER(CONCAT('%', :search, '%')) " +
                     "OR LOWER(p.description) LIKE LOWER(CONCAT('%', :search, '%')) " +
                     "OR LOWER(pv.sku) LIKE LOWER(CONCAT('%', :search, '%')) " +
-                    "OR LOWER(pv.barcode) LIKE LOWER(CONCAT('%', :search, '%'))",
+                    "OR LOWER(pv.barcode) LIKE LOWER(CONCAT('%', :search, '%'))) " +
+                    "ORDER BY p.last_modified_date DESC",
+            countQuery = "SELECT COUNT(DISTINCT p.id) FROM products p " +
+                    "LEFT JOIN product_variants pv ON p.id = pv.product_id " +
+                    "LEFT JOIN product_categories pc ON p.id = pc.product_id " +
+                    "WHERE (:status IS NULL OR p.status = :status) " +
+                    "AND (:categoryId IS NULL OR pc.category_id = :categoryId) " +
+                    "AND (LOWER(p.product_name) LIKE LOWER(CONCAT('%', :search, '%')) " +
+                    "OR LOWER(p.description) LIKE LOWER(CONCAT('%', :search, '%')) " +
+                    "OR LOWER(pv.sku) LIKE LOWER(CONCAT('%', :search, '%')) " +
+                    "OR LOWER(pv.barcode) LIKE LOWER(CONCAT('%', :search, '%')))",
             nativeQuery = true)
-    Page<Product> searchProduct(String search, Integer categoryId, String status, Pageable pageable);
+    Page<Product> searchProduct(
+            @Param("search") String search,
+            @Param("categoryId") Integer categoryId,
+            @Param("status") String status,
+            Pageable pageable
+    );
 }
