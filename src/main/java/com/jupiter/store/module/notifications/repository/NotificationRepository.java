dc0aa7ad3dbff55cd2cd27f1e7f4d171a81ec4f0
Camtu <>
2025-06-10 00:28:01 +0700

    fix: không gửi thông báo đổi mật khẩu & lỗi lệch múi giờ

--- src/main/java/com/jupiter/store/module/notifications/repository/NotificationRepository.java
+++ src/main/java/com/jupiter/store/module/notifications/repository/NotificationRepository.java
@@ -10,15 +10,16 @@ import java.util.List;
 import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    @Query(value = "SELECT * FROM notifications n WHERE n.user_id = :userId " +
-            "ORDER BY n.date DESC " +
-            "OFFSET :page * 10 LIMIT 10 ", nativeQuery = true)
+    @Query(value = "SELECT * FROM notifications n WHERE n.user_id = :userId AND n.entity_type != 'PASSWORD_RESET' " +
+                    "ORDER BY n.date DESC " +
+                    "OFFSET :page * 10 LIMIT 10 ", nativeQuery = true)
     List<Notification> findByUserId(@Param("userId") Integer userId, @Param("page") Integer page);
 
 
-    @Query(value = "SELECT * FROM notifications n WHERE n.entity_type = 'PASSWORD_RESET' " +
+    @Query(value = "SELECT * FROM notifications n WHERE n.user_id = :userId " +
+                    "AND n.entity_type = 'PASSWORD_RESET' " +
                     "AND n.entity_id = :otp " +
-                    "AND n.date >= :nowMinus5 ", nativeQuery = true)
+                    "AND n.date >= NOW() - INTERVAL '7 hour' - INTERVAL '5 minute' ", nativeQuery = true)
     Optional<Notification> verifyByEntityId(
             @Param("otp") Integer otp,
             @Param("nowMinus5")LocalDateTime nowMinus5);
