ca79386c9ceec1000d9cad52eb1e744bb44e177b
Nguyen Duc Tam <tamgtasa2611@gmail.com>
2025-06-12 20:11:44 +0700

    feat: xóa THANH_TOAN_CO_THE_BI_GIA_MAO

--- src/main/java/com/jupiter/store/module/order/constant/PaymentStatus.java
+++ src/main/java/com/jupiter/store/module/order/constant/PaymentStatus.java
@@ -3,6 +3,5 @@ package com.jupiter.store.module.order.constant;
 public enum PaymentStatus {
     CHUA_THANH_TOAN,
     THANH_TOAN_THANH_CONG,
-    THANH_TOAN_THAT_BAI,
-    THANH_TOAN_CO_THE_BI_GIA_MAO
+    THANH_TOAN_THAT_BAI
 }
